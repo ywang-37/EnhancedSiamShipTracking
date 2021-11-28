a0efbf8e90ff3ec8d9bf1a985a32c52246b9ba52
Kong <54091984+ywang-37@users.noreply.github.com>
2021-11-28 21:49:05 +0800

    Update run.sh

--- experiments/EnhancedSiamShipTracking/run.sh
+++ experiments/EnhancedSiamShipTracking/run.sh
@@ -4,10 +4,10 @@ export PYTHONPATH=$ROOT:$PYTHONPATH
 mkdir -p logs
 
 python -u $ROOT/tools/train_siammask.py \
-    --config=config.json -b 64 \
-    -j 20 --resume snapshot/checkpoint_e9.pth\
+    --config=config.json -b 16 \
+    -j 20 \
     --epochs 20 \
     --log logs/log.txt \
     2>&1 | tee logs/train.log
 
-bash test_all.sh -s 1 -e 20 -d VOT2018 -g 4
+# bash test_all.sh -s 1 -e 20 -d VOT2018 -g 4
